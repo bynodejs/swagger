43c79a2513753221c34960d39de1bfe804638567
임경민 <ljlm0402@gmail.com>
2019-10-01 12:18:23 +0900

    ctrl fix

--- controllers/boards.ctrl.js
+++ controllers/boards.ctrl.js
@@ -1,64 +1,65 @@
-'use strict';
+"use strict";
 
-const BoardModel = require('../models/boards.model');
+const BoardModel = require("../models/boards.model");
 
-const error = require('../lib/error');
+const error = require("../lib/error");
 
 module.exports = {
+  get_board_list() {
+    return new Promise((resolve, reject) => {
+      try {
+        if (!BoardModel.length) reject(error.get(204, "No Content"));
+        resolve(BoardModel);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
 
-    get_board_list() {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!BoardModel.length) reject(error.get(204, 'No Content'));
-                resolve(BoardModel);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
+  get_board_detail(board_id) {
+    return new Promise((resolve, reject) => {
+      try {
+        const board = BoardModel.filter(board => board.board_id == board_id);
+        if (!board.length) reject(error.get(204, "No Content"));
+        resolve(board);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
 
-    get_board_detail(board_id) {
-        return new Promise((resolve, reject) => {
-            try {
-                const board = BoardModel.filter(board => board.board_id == board_id);
-                if (!board.length) reject(error.get(204, 'No Content'));
-                resolve(board);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
+  create_board(data) {
+    return new Promise((resolve, reject) => {
+      try {
+        if (!data) reject(error.get(400, "Bad Request"));
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
 
-    create_board(data) {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!data) reject(error.get(400, 'Bad Request'));
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
+  update_board(board_id, data) {
+    return new Promise((resolve, reject) => {
+      try {
+        const board = BoardModel.filter(board => board.board_id == board_id);
+        if (!board.length || !data) reject(error.get(400, "Bad Request"));
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
 
-    update_board(board_id, data) {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!board_id || !data) reject(error.get(400, 'Bad Request'));
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
-
-    delete_board(board_id) {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!board_id) reject(error.get(400, 'Bad Request'));
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
-}
+  delete_board(board_id) {
+    return new Promise((resolve, reject) => {
+      try {
+        const board = BoardModel.filter(board => board.board_id == board_id);
+        if (!board.length) reject(error.get(400, "Bad Request"));
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+};
--- controllers/users.ctrl.js
+++ controllers/users.ctrl.js
@@ -1,64 +1,65 @@
-'use strict';
+"use strict";
 
-const UserModel = require('../models/users.model');
+const UserModel = require("../models/users.model");
 
-const error = require('../lib/error');
+const error = require("../lib/error");
 
 module.exports = {
+  get_user_list() {
+    return new Promise((resolve, reject) => {
+      try {
+        if (!UserModel.length) resolve(error.get(204, "No Content"));
+        resolve(UserModel);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
 
-    get_user_list() {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!UserModel.length) resolve(error.get(204, 'No Content'));
-                resolve(UserModel);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
+  get_user_detail(user_id) {
+    return new Promise((resolve, reject) => {
+      try {
+        const user = UserModel.filter(user => user.user_id == user_id);
+        if (!user.length) resolve(error.get(204, "No Content"));
+        resolve(user);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
 
-    get_user_detail(user_id) {
-        return new Promise((resolve, reject) => {
-            try {
-                const user = UserModel.filter(user => user.user_id == user_id);
-                if (!user.length) resolve(error.get(204, 'No Content'));
-                resolve(user);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
+  create_user(data) {
+    return new Promise((resolve, reject) => {
+      try {
+        if (!data) reject(error.get(400, "Bad Request"));
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
 
-    create_user(data) {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!data) reject(error.get(400, 'Bad Request'));
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
+  update_user(user_id, data) {
+    return new Promise((resolve, reject) => {
+      try {
+        const user = UserModel.filter(user => user.user_id == user_id);
+        if (!user.length || !data) reject(error.get(400, "Bad Request"));
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
 
-    update_user(user_id, data) {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!user_id || !data) reject(error.get(400, 'Bad Request'));
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
-
-    delete_user(user_id) {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!user_id) reject(error.get(400, 'Bad Request'));
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
-}
+  delete_user(user_id) {
+    return new Promise((resolve, reject) => {
+      try {
+        const user = UserModel.filter(user => user.user_id == user_id);
+        if (!user.length) reject(error.get(400, "Bad Request"));
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+};
